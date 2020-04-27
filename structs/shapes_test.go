592a3156d6cabc1966c3249da201789467e2702b
maksmaggot <maksmaggot@gmail.com>
2020-04-27 19:01:02 +0700

    shape Triangle added

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -30,4 +30,19 @@ func TestArea(t *testing.T) {
 		circle := Circle{10}
 		checkArea(t, circle, 314.1592653589793)
 	})
+
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
 }
