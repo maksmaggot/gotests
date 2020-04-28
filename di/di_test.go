4c05f20ccba5ab5902c6ad999bca5af97ebbe9ec
maksmaggot <maksmaggot@gmail.com>
2020-04-28 14:05:07 +0700

    di

--- di/di_test.go
+++ di/di_test.go
@@ -1,1 +1,18 @@
 package di
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Max")
+
+	got := buffer.String()
+	want := "Hello, Max"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
